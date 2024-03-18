da8d96df047fc93da1375e8a1119fd206b0caacd
Rohan Rusho <111059342+rohan-rusho@users.noreply.github.com>
2024-03-18 21:46:23 +0600

    Add files via upload

---
+++ E-Home_Pro/src/main/java/com/mycompany/e/home_pro/EHome_Pro.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.e.home_pro;
+
+
+
+/**
+ *
+ * @author ahmed rohan
+ */
+public class EHome_Pro {
+
+    public static void main(String args[]) {
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LoginPage().setVisible(true);
+            }
+        });
+    }
+}
